5476e478ec2ec56f22e1e488af21d9c5cecf36e8
ｒｙｍｅ <119275831+irymee@users.noreply.github.com>
2023-03-06 14:12:15 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,49 @@
+from pyrogram import Client, filters
+from pyrogram.types import *
+from os import env
+
+CHAT_ID = get.env("CHAT_ID")
+API_ID = get.env("API_ID")
+API_HASH = get.env("API_HASH")
+BOT_TOKEN = get.env("BOT_TOKEN")
+
+app = Client(bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+
+TEXT = "Hello {}, Welcome To {}"
+APPROVED = True  # set to True by default
+
+# auto approve members 
+@Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
+async def autoapprove(client: Client, message: ChatJoinRequest):
+    chat=message.chat
+    user=message.from_user
+    print(f"{user.first_name} Joined")
+    if APPROVED:
+        await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+        await client.send_message(chat_id=chat.id, text=TEXT.format(user.mention, chat.title))
+
+# enable/disable auto approve feature
+@Client.on_message(filters.command("autoapprove") & filters.private)
+async def toggle_autoapprove(client: Client, message: Message):
+    chat = message.chat
+    user_id = message.from_user.id
+    member = await client.get_chat_member(chat_id=chat.id, user_id=user_id)
+    if member.can_manage_chat:
+        global APPROVED
+        if len(message.command) > 1:
+            action = message.command[1].lower()
+            if action == "on":
+                APPROVED = True
+                await message.reply("Auto approve is now enabled.")
+            elif action == "off":
+                APPROVED = False
+                await message.reply("Auto approve is now disabled.")
+            else:
+                await message.reply("Invalid argument. Use 'on' or 'off'.")
+        else:
+            await message.reply(f"Auto approve is currently {'enabled' if APPROVED else 'disabled'}. Use 'on' or 'off' to toggle.")
+    else:
+        await message.reply("Only group admins can enable or disable auto approve.")
+
+
+app.run()
