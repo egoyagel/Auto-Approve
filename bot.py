382dd73d5e89f96a5e1136adea58e7c2f96ca3bb
egoyagel <egoyagel@gmail.com>
2023-07-15 22:28:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,12 @@ API_ID = os.getenv("23776178")
 API_HASH = os.getenv("b69598ab1d4021eb968d2db5b8bed30a")
 BOT_TOKEN = os.getenv("6323849431:AAHX8uFm54ZkJcSD2hmnoLlxO1lvLaunP3A")
 
-app = Client(bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+app = Client(
+    session_name="session_name.session",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
+)
 
 TEXT = "Hello {}, Welcome To {}"
 APPROVED = True  # set to True by default
@@ -32,9 +37,11 @@ async def toggle_autoapprove(client: Client, message: Message):
         if len(message.command) > 1:
             action = message.command[1].lower()
             if action == "on":
+                global APPROVED
                 APPROVED = True
                 await message.reply("Auto approve is now enabled.")
             elif action == "off":
+                global APPROVED
                 APPROVED = False
                 await message.reply("Auto approve is now disabled.")
             else:
@@ -43,6 +50,5 @@ async def toggle_autoapprove(client: Client, message: Message):
             await message.reply(f"Auto approve is currently {'enabled' if APPROVED else 'disabled'}. Use 'on' or 'off' to toggle.")
     else:
         await message.reply("Only group admins can enable or disable auto approve.")
-
 
 app.run()
